1757f04979e5c108931358fb66eee362a4a4871d
HIFZI FURKAN KUTAY <furkankutay06@gmail.com>
2022-12-23 22:29:49 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const cookie = require("borek")
+
+module.exports.set = function(cname, cvalue) {
+  document.cookie = cname + "=" + cvalue + ";path=/";
+}
+
+module.exports.get = function(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';') ? document.cookie.split(';') : "" 
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+module.exports.check = function() {
+  let user = get("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      set("username", user, 365);
+    }
+  }
+}
